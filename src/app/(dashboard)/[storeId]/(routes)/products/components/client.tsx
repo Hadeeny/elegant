e95d344206edcce32d521a7bf67f4fc21a867d69
hadeeny <oluwasegunadeniyi064@gmail.com>
2024-05-06 01:02:30 +0100

    product client optimized

--- src/app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -1,23 +1,56 @@
-"use client";
+// "use client";
 
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import Heading from "@/components/ui/heading";
 import { Divider } from "@nextui-org/react";
 import { Billboard } from "@prisma/client";
 import { Divide, Plus } from "lucide-react";
-import { useParams, useRouter } from "next/navigation";
+// import { useParams, useRouter } from "next/navigation";
 import React from "react";
 import { ProductColumn, columns } from "./columns";
 import { DataTable } from "@/components/ui/data-tables";
 import { ApiList } from "@/components/ui/api-list";
+import Link from "next/link";
+import { db } from "@/lib/db";
+import { format } from "date-fns";
+import { unstable_noStore as noStore } from "next/cache";
+import { formatPrice, formatter } from "@/lib/utils";
+// import { ProductColumn } from "./components/columns";
 
-interface ProductClientProps {
-  data: ProductColumn[];
-}
+export const ProductClient = async ({
+  params,
+}: {
+  params: { storeId: string };
+}) => {
+  // const params = useParams();
+  // console.log(params.storeId);
+  noStore();
+  const products = await db.product.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    include: {
+      category: true,
+      color: true,
+      size: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
 
-export const ProductClient: React.FC<ProductClientProps> = ({ data }) => {
-  const router = useRouter();
-  const params = useParams();
+  const data: ProductColumn[] = products.map((item) => ({
+    id: item.id,
+    name: item.name,
+    isFeatured: item.isFeatured,
+    isArchived: item.isArchived,
+    price: formatter.format(item.price.toNumber()),
+    category: item.category.name,
+    size: item.size.name,
+    color: item.color.value,
+    createdAt: format(item.createdAt, "MMMM do, yyy"),
+  }));
+
   return (
     <>
       <div className="flex items-center justify-between">
@@ -25,9 +58,14 @@ export const ProductClient: React.FC<ProductClientProps> = ({ data }) => {
           title={`Products (${data.length})`}
           description="Manage products for your store"
         />
-        <Button onClick={() => router.push(`/${params.storeId}/products/new`)}>
+        <Link
+          className={buttonVariants({
+            className: "",
+          })}
+          href={`/${params.storeId}/products/new`}
+        >
           <Plus className="mr-2 h-4 w-4" /> Add New
-        </Button>
+        </Link>
       </div>
       <Divider />
       {/* <DataTable searchKey="label" columns={columns} data={data} /> */}
--- src/app/(dashboard)/[storeId]/(routes)/products/page.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -7,37 +7,11 @@ import { formatPrice, formatter } from "@/lib/utils";
 import { ProductColumn } from "./components/columns";
 
 const ProductsPage = async ({ params }: { params: { storeId: string } }) => {
-  noStore();
-  const products = await db.product.findMany({
-    where: {
-      storeId: params.storeId,
-    },
-    include: {
-      category: true,
-      color: true,
-      size: true,
-    },
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
-
-  const formattedProducts: ProductColumn[] = products.map((item) => ({
-    id: item.id,
-    name: item.name,
-    isFeatured: item.isFeatured,
-    isArchived: item.isArchived,
-    price: formatter.format(item.price.toNumber()),
-    category: item.category.name,
-    size: item.size.name,
-    color: item.color.value,
-    createdAt: format(item.createdAt, "MMMM do, yyy"),
-  }));
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-4 sm:p-8 pt-6">
         <Suspense fallback={<p>Loading billboard client</p>}>
-          <ProductClient data={formattedProducts} />
+          <ProductClient params={params} />
         </Suspense>
       </div>
     </div>
