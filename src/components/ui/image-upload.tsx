0c343571bfbe0649fb59797802c9389c50de60c8
hadeeny <oluwasegunadeniyi064@gmail.com>
2024-05-07 17:39:02 +0100

    bug fixed

--- src/components/ui/image-upload.tsx
+++ src/components/ui/image-upload.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ImagePlus, Trash, Upload } from "lucide-react";
+import { ImagePlus, Trash } from "lucide-react";
 import { useEffect, useState } from "react";
 import { Button } from "./button";
 import Image from "next/image";
@@ -33,25 +33,23 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
   return (
     <>
       <div>
-        <div className="grid grid-cols-2 gap-2">
+        <div className="mb-4 flex items-center gap-4">
           {value.map((url) => (
-            <div key={url} className="relative">
-              <Button
-                type="button"
-                onClick={() => onRemove(url)}
-                variant="destructive"
-                size="sm"
-                className="absolute top-0"
-              >
-                <Trash className="h-4 w-4" />
-              </Button>
-              <Image
-                alt="Product image"
-                className="aspect-square w-full rounded-md object-cover"
-                height="84"
-                src={url}
-                width="84"
-              />
+            <div
+              key={url}
+              className="relative size-[250px] rounded-md overflow-hidden"
+            >
+              <div className="z-10 absolute top-2 right-2">
+                <Button
+                  type="button"
+                  onClick={() => onRemove(url)}
+                  variant="destructive"
+                  size="sm"
+                >
+                  <Trash className="h-4 w-4" />
+                </Button>
+              </div>
+              <Image fill className="object-cover" alt="Image" src={url} />
             </div>
           ))}
         </div>
@@ -62,14 +60,15 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
             };
 
             return (
-              <button
-                onClick={onClick}
+              <Button
+                type="button"
                 disabled={disabled}
-                className="flex aspect-square w-14 mt-5 items-center justify-center rounded-md border border-dashed"
+                variant="secondary"
+                onClick={onClick}
               >
-                <Upload className="h-4 w-4 text-muted-foreground" />
-                <span className="sr-only">Upload</span>
-              </button>
+                <ImagePlus className="h-4 w-4 mr-2" />
+                Upload an Image
+              </Button>
             );
           }}
         </CldUploadWidget>
@@ -77,18 +76,5 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
     </>
   );
 };
-
-{
-  /* <div className="grid gap-2">
-                            <Image
-                        alt="Product image"
-                        className="aspect-square w-full rounded-md object-cover"
-                        height="300"
-                        src="/placeholder.svg"
-                        width="300"
-                      />
-                            
-                          </div> */
-}
 
 export default ImageUpload;
